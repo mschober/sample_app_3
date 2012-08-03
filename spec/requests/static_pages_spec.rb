c7de7b5cbcc7c333e2f629ed0e308be3a0b4391f
Mike Schober <mschober@cj.com>
2012-08-02 20:22:55 -0700

    updated tests to include title chnages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
