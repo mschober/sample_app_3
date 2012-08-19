d97b85360433aad378fec2d1ae68923b8908e86d
Mike Schober <mschober@cj.com>
2012-08-18 19:13:20 -0700

    adding a user model and annotation support

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ group :development, :test do
   gem 'rspec-rails', '2.10.0'
 end
 
+gem 'annotate', '2.5.0', group: :development
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+#describe User do
+#
+#  before { @user = User.new(name: "Example User", email: "user@example.com") }
+#
+#  subject { @user }
+#
+#  it { should respond_to(:name) }
+#  it { should respond_to(:email) }
+#end
+
+describe User do
+
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+end
+
