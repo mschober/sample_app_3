c04f09afed32fed2b1844f7399adaf6d5a0bd9ca
Mike Schober <mschober@cj.com>
2012-08-20 08:48:41 -0700

    signin form with tests and invalid information flasher

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+    else
+      flash[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
   end
 
   def destroy
