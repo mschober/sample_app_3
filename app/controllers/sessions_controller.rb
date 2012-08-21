e12d799ac16be3f9b3ac0320ad3ffdb36b958458
Mike Schober <mschober@cj.com>
2012-08-20 18:43:08 -0700

    adding a remember_token to users table, + index and tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination' # Not quite right!
       render 'new'
