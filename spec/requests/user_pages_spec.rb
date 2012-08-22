901a3c83801601b8a18d29dfbb1dd1cdc43eadb7
Mike Schober <mschober@cj.com>
2012-08-21 20:32:23 -0700

    finished signing out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,6 +48,12 @@ describe "UserPages" do
         let(:user) { User.find_by_email("user@example.com") }
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+
+        describe "followed by signout" do
+          before { click_link 'Sign out' }
+          it { should have_link('Sign in') }
+        end
       end
     end
   end
