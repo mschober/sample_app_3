dc3aaf5a0e9b28da5c0936a9b63e3942ccd91e01
Mike Schober <mschober@cj.com>
2012-08-25 16:08:27 -0700

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   private
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,16 @@
+#FactoryGirl.define do
+#  factory :user do
+#    name     "Michael Hartl"
+#    email    "michael@example.com"
+#    password "foobar"
+#    password_confirmation "foobar"
+#  end
+#end
+
 FactoryGirl.define do
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
   end
