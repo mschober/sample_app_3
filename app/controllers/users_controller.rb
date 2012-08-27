e75df48cd0cb12803921d94fef09f75f35296724
Mike Schober <mschober@cj.com>
2012-08-26 20:53:25 -0700

    microposts controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,11 +48,6 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_path unless current_user?(@user)
