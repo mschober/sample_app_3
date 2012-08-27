c96d1e61b849be2b66363c210e398291f3270217
Mike Schober <mschober@cj.com>
2012-08-26 21:15:58 -0700

    dual headed home page with post box

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,6 +5,13 @@ class MicropostsController < ApplicationController
   end
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
