038d1407a38ca6867396088fdd9969374d5fd914
Mike Schober <mschober@cj.com>
2012-08-27 19:02:49 -0700

    testing relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,9 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+
   def feed
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Relationship do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:follower) { FactoryGirl.create(:user) }
+  let(:followed) { FactoryGirl.create(:user) }
+  let(:relationship) { follower.relationships.build(followed_id: followed.id) }
+
+  subject { relationship }
+
+  it { should be_valid }
+
+  describe "accessible attributes" do
+    it "should not all access to follower_id" do
+      expect do
+        Relationship.new(follower_id: follower.id)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+  end
+
+  describe "follower methods" do
+    it { should respond_to(:follower) }
+    it { should respond_to(:followed) }
+    its(:follower) { should == follower }
+    its(:followed) { should == followed }
+  end
 end
