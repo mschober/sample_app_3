62336cae85c954535f401bc540b8a96daa1304f4
Mike Schober <mschober@cj.com>
2012-08-27 21:00:37 -0700

    updating the database & routes for followers/following

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 SampleApp::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
 
